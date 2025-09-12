d472b0119f62efdc90d29aca63f561aa987fe90b
Oleg Morgun <raubtierm@gmail.com>
2025-09-13 00:53:34 +0700

    Практикуемся в использовании API клиентов

---
+++ api_client_create_exercise.py
@@ -0,0 +1,90 @@
+from clients.courses.courses_client import get_courses_client, CreateCourseRequestDict
+from clients.exercises.exercises_client import get_exercises_client, CreateExerciseRequestDict, GetExercisesQueryDict, \
+    UpdateExerciseRequestDict
+from clients.files.files_client import get_files_client, CreateFileRequestDict
+from clients.private_http_builder import AuthenticationUserDict
+from clients.users.public_users_client import get_public_users_client, CreateUserRequestDict
+from tools.fakers import get_random_email
+
+public_users_client = get_public_users_client()
+
+# Создаем пользователя
+create_user_request = CreateUserRequestDict(
+    email=get_random_email(),
+    password="string",
+    lastName="string",
+    firstName="string",
+    middleName="string"
+)
+create_user_response = public_users_client.create_user(create_user_request)
+
+# Инициализируем клиенты на создание файлов и курсов
+authentication_user = AuthenticationUserDict(
+    email=create_user_request['email'],
+    password=create_user_request['password']
+)
+files_client = get_files_client(authentication_user)
+courses_client = get_courses_client(authentication_user)
+
+# Загружаем файл
+create_file_request = CreateFileRequestDict(
+    filename="image.png",
+    directory="courses",
+    upload_file="./testdata/files/image.png"
+)
+create_file_response = files_client.create_file(create_file_request)
+print('Create file data:', create_file_response)
+
+# Создаем курс
+create_course_request = CreateCourseRequestDict(
+    title="Python",
+    maxScore=100,
+    minScore=10,
+    description="Python API course",
+    estimatedTime="2 weeks",
+    previewFileId=create_file_response['file']['id'],
+    createdByUserId=create_user_response['user']['id']
+)
+create_course_response = courses_client.create_course(create_course_request)
+print('Create course data:', create_course_response)
+
+# Создаем задание
+exercises_client = get_exercises_client(authentication_user)
+create_exercise_request = CreateExerciseRequestDict(
+    title="Exercise 1",
+    courseId=create_course_response['course']['id'],
+    maxScore=5,
+    minScore=1,
+    orderIndex=0,
+    description="Exercise 1",
+    estimatedTime="5 minutes"
+)
+create_exercise_response = exercises_client.create_exercise(create_exercise_request)
+print('Create exercise data:', create_exercise_response)
+
+# Получение списка заданий (тестовое)
+get_exercises_request = GetExercisesQueryDict(
+    courseId=create_course_response['course']['id']
+)
+get_exercises_response = exercises_client.get_exercises(get_exercises_request)
+print('Get exercises list:', get_exercises_response['exercises'])
+
+# Получение информации о задании (тестовое)
+exercise_id = create_exercise_response['exercise']['id']
+get_exercise_response = exercises_client.get_exercise(exercise_id)
+print('Get exercise data:', get_exercise_response)
+
+# Обновление задания (тестовое)
+update_exercise_request = UpdateExerciseRequestDict(
+    title="Exercise 1.1",
+    maxScore=5,
+    minScore=1,
+    orderIndex=0,
+    description="Updated exercise 1",
+    estimatedTime="5 minutes"
+)  # Из-за ошибки 500 необходимо указание всех полей в запросе, включая неизменяемые
+update_exercise_response = exercises_client.update_exercise(
+    exercise_id,
+    update_exercise_request
+)
+print('Update exercise data:', update_exercise_response)
