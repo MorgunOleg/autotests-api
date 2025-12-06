0327e1101d2f4c293ea4fde7e193aeff9f4f0e93
Oleg Morgun <raubtierm@gmail.com>
2025-12-06 16:24:51 +0700

    Практикуемся в написании API автотестов. Часть 3

--- tests/exercises/test_exercises.py
+++ tests/exercises/test_exercises.py
@@ -2,6 +2,7 @@ from http import HTTPStatus
 
 import pytest
 
+from clients.errors_schema import InternalErrorResponseSchema
 from clients.exercises.exercises_client import ExercisesClient
 from clients.exercises.exercises_schema import (
     CreateExerciseRequestSchema,
@@ -14,7 +15,7 @@ from fixtures.courses import CourseFixture
 from fixtures.exercises import function_exercise, ExerciseFixture
 from tools.assertions.base import assert_status_code
 from tools.assertions.exercises import assert_create_exercise_response, assert_get_exercise_response, \
-    assert_update_exercise_response
+    assert_update_exercise_response, assert_exercise_not_found_response
 from tools.assertions.schema import validate_json_schema
 
 
@@ -63,3 +64,18 @@ class TestExercises:
         assert_status_code(response.status_code, HTTPStatus.OK)
         # Проверяем, что данные в ответе соответствуют запросу
         assert_update_exercise_response(request, response_data)
+
+    def test_delete_exercise(self, exercises_client: ExercisesClient, function_exercise: ExerciseFixture):
+        # Отправляем DELETE-запрос на удаление задания
+        delete_response = exercises_client.delete_exercise_api(exercise_id=function_exercise.response.exercise.id)
+        # Проверяем, что файл успешно удалено (статус-код ответа 200 OK)
+        assert_status_code(delete_response.status_code, HTTPStatus.OK)
+
+        # Проверяем попытку получить удаленное задание
+        get_response = exercises_client.get_exercise_api(function_exercise.response.exercise.id)
+        assert_status_code(get_response.status_code, HTTPStatus.NOT_FOUND)
+        get_response_data = InternalErrorResponseSchema.model_validate_json(get_response.text)
+        assert_exercise_not_found_response(get_response_data)
+
+        # Валидируем JSON-схему ответа на GET-запрос
+        validate_json_schema(get_response.json(), get_response_data.model_json_schema())
