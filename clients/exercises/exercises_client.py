3404e5a30e8e26d75d9b84d716fd1c76b34055a5
Oleg Morgun <raubtierm@gmail.com>
2026-02-07 21:25:12 +0700

    Практикуемся в интеграции покрытия в API тесты

--- clients/exercises/exercises_client.py
+++ clients/exercises/exercises_client.py
@@ -2,6 +2,7 @@ import allure
 from httpx import Response
 
 from clients.api_client import APIClient
+from clients.api_coverage import tracker
 from clients.exercises.exercises_schema import (
     GetExerciseResponseSchema,
     GetExercisesQuerySchema,
@@ -21,6 +22,8 @@ class ExercisesClient(APIClient):
     """
 
     @allure.step("Get exercises")
+    # Добавили сбор покрытия для эндпоинта GET /api/v1/exercises
+    @tracker.track_coverage_httpx(APIRoutes.EXERCISES)
     def get_exercises_api(self, query: GetExercisesQuerySchema) -> Response:
         """
         Метод получения списка заданий определенного курса.
@@ -31,6 +34,8 @@ class ExercisesClient(APIClient):
         return self.get(APIRoutes.EXERCISES, params=query.model_dump(by_alias=True))
 
     @allure.step("Get exercise by id {exercise_id}")
+    # Добавили сбор покрытия для эндпоинта GET /api/v1/exercises/{exercise_id}
+    @tracker.track_coverage_httpx(f"{APIRoutes.EXERCISES}/{{exercise_id}}")
     def get_exercise_api(self, exercise_id: str) -> Response:
         """
         Метод получения информации о задании по exercise_id.
@@ -41,6 +46,8 @@ class ExercisesClient(APIClient):
         return self.get(f"{APIRoutes.EXERCISES}/{exercise_id}")
 
     @allure.step("Create exercise")
+    # Добавили сбор покрытия для эндпоинта POST /api/v1/exercises
+    @tracker.track_coverage_httpx(APIRoutes.EXERCISES)
     def create_exercise_api(self, request: CreateExerciseRequestSchema) -> Response:
         """
         Метод создания задания.
@@ -52,6 +59,8 @@ class ExercisesClient(APIClient):
         return self.post(APIRoutes.EXERCISES, json=request.model_dump(by_alias=True))
 
     @allure.step("Update exercise by id {exercise_id}")
+    # Добавили сбор покрытия для эндпоинта PATCH /api/v1/exercises/{exercise_id}
+    @tracker.track_coverage_httpx(f"{APIRoutes.EXERCISES}/{{exercise_id}}")
     def update_exercise_api(self, exercise_id: str, request: UpdateExerciseRequestSchema) -> Response:
         """
         Метод обновления данных задания.
@@ -64,6 +73,8 @@ class ExercisesClient(APIClient):
         return self.patch(f"{APIRoutes.EXERCISES}/{exercise_id}", json=request.model_dump(by_alias=True))
 
     @allure.step("Delete exercise by id {exercise_id}")
+    # Добавили сбор покрытия для эндпоинта DELETE /api/v1/exercises/{exercise_id}
+    @tracker.track_coverage_httpx(f"{APIRoutes.EXERCISES}/{{exercise_id}}")
     def delete_exercise_api(self, exercise_id: str) -> Response:
         """
         Метод удаления задания.
